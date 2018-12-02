c80604cf35f14ba9a584b136e8bba6ae453915e1
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-12-02 16:04:51 +0300

    Create tast4.2.2.py

---
+++ Invar_tasks/tast4.2.2.py
@@ -0,0 +1,16 @@
+#4.2. Создание программы по распределению списка с случайными значениями на два списка 
+по определенному критерию четность/нечетность.
+
+import random
+array = []
+
+for i in range(50):
+    array.append(int(random.random() * 100))
+    
+array.sort()
+
+even_num = list(filter(lambda x: not x%2, array))
+odd_num = list(filter(lambda x: x%2, array))
+
+print(even_num)
+print(odd_num)
