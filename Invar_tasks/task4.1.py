94726617525320473b79db8a77da435513c154c5
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-12-02 15:55:37 +0300

    Create task4.1.py

---
+++ Invar_tasks/task4.1.py
@@ -0,0 +1,50 @@
+#Инвариантная СР
+#4.1. Создание программы по заполнению массивов случайными значениями. 
+#Сортировка значений в списке методом вставки, плавной сортировки, с помощью встроенных функций языка.
+
+#NumPy — это библиотека языка Python, добавляющая поддержку больших 
+#многомерных массивов и матриц, вместе с большой библиотекой 
+#высокоуровневых (и очень быстрых) математических функций 
+#для операций с этими массивами.
+
+#Для создания массивов со случайными элементами служит модуль numpy.random
+
+"""Создаем массив со случайными эелементами"""
+import numpy as np
+import numpy.random 
+
+"""создание массива со случайными целыми и дробными числами"""
+float_array = np.random.sample(10)
+array = np.random.randint(0, 12, 10)
+a = np.arange(10)
+
+"""Реализация различных сортировок:
+      1) сортировка по возрастанию/убыванию;
+      2) случайная выборка из массива, с помощью функции choice;
+      3) функция permutation позволяет перемешать и возвратить полученный массив.
+"""
+sort_array = sorted(array)
+reverse_array = sorted(array, reverse = True)
+
+two_array = np.random.choice(a, 20, replace = True)
+new_array = np.random.permutation(array)
+
+#print(float_array)
+#print(array)
+#print(new_array)
+#print(two_array)
+#print(sort_array)
+#print(reverse_array)
+
+"""Сортировка простыми вставками"""
+def just_sort(lst = np.random.randint(0, 12, 10)):
+    for i in range(len(lst) - 1):
+        j = i - 1 
+        key = lst[i]
+        while lst[j] > key and j >= 0:
+            lst[j + 1] = lst[j]
+            j -= 1
+        lst[j + 1] = key
+    return lst
+
+just_sort()
