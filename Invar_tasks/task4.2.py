50a6a3c20112e48d78f317cdcc07e9e78cb2c66e
Kseniaveh <31632695+Kseniaveh@users.noreply.github.com>
2018-12-02 16:03:49 +0300

    Create task4.2.py

---
+++ Invar_tasks/task4.2.py
@@ -0,0 +1,23 @@
+#Инвариантная СР
+#4.2. Создание программы по распределению списка с случайными значениями на два списка 
+#по определенному критерию четность/нечетность.
+
+import random
+array = []
+
+for i in range(10):
+    array.append(int(random.random() * 10) - 5)
+
+negative_num = []
+positive_num = []
+
+for i in array:
+    if i < 0:
+        negative_num.append(i)
+    elif i > 0:
+        positive_num.append(i)
+        
+print(array)
+print(negative_num)
+print(positive_num)
+
